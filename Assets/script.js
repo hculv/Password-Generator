bb8cd94cc086813348a6e1d932946019cf2d6cd7
Hannah Culver-Zawislak <95580453+hculv@users.noreply.github.com>
2022-05-22 11:41:23 -0400

    new script.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,38 +1,39 @@
 
-var specChars = ['!', '#', '$', '%', '&', '(', ')', '*', ',', '-', '.', '/', ':', ';', '<', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
-var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var button = document.querySelector("#generate");
 
+var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var specChars = ['!', '#', '$', '%', '&', '(', ')', '*', ',', '-', '.', '/', ':', ';', '<', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
 
+var numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
-function generateRandomNumber (min,max) {
+var chars = []
+var userOutput = []
+var charLength = 8;
+
+function generateRandomString (min,max) {
     var value = Math.floor(Math.random() * (max - min + 1) + min);
     return value;
   };
 
 function printPassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  var passwordEntry = document.querySelector("#password");
+  passwordEntry.value = password;
 
 
 
 function generatePassword () {
-var chars = []
-var userOutput = []
-var charAmount = 8;
-var charAmount = window.prompt("How many characters would you like in your password?")
-charAmount = parseInt(charAmount)
+var charLength = window.prompt("How many characters would you like in your password?")
+charLength = parseInt(charLength)
 
 
-while ( charAmount > 128 || charAmount <= 8 || isNaN(charAmount)) {
+while ( charLength > 128 || charLength <= 8 || isNaN(charLength)) {
   alert("Password needs to be between 8 and 28 characters.")
-  var charAmount = window.prompt("How many characters would you like in your password?")
-  charAmount = parseInt(charAmount)
+  var charLength = window.prompt("How many characters would you like in your password?")
+  charLength = parseInt(charLength)
 }
+
 
 
 
@@ -58,10 +59,8 @@ if (userLower) {
   chars = chars.concat(lowerCase)
 };
 
-
-
-for (var i = 0; i < charAmount; i++) {
-  userOutput = userOutput.concat(chars[generateRandomNumber(1, chars.length)])
+for (var i = 0; i < charLength; i++) {
+  userOutput = userOutput.concat(chars[generateRandomString(1, chars.length)])
 };
 return (userOutput.join(""));
 };
